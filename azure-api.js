d0dbe330ebaaa7ec3c34cff7b5336cc1cb233663
Shad0w30 <51172311+Shad0w30@users.noreply.github.com>
2026-01-21 00:02:05 +0400

    Refactor API functions to use internal token retrieval
    Refactor armGet, getAKS, and getACR functions to remove token parameter and retrieve token internally.

--- azure-api.js
+++ azure-api.js
@@ -1,30 +1,24 @@
-async function armGet(url, token) {
+async function armGet(url) {
+  const token = window.armToken || document.getElementById("token").value;
+
   const res = await fetch(url, {
     headers: {
       "Authorization": `Bearer ${token}`,
       "Content-Type": "application/json"
     }
   });
+
   return res.json();
 }
 
-async function getAKS(subId, token) {
+async function getAKS(sub) {
   return armGet(
-    `https://management.azure.com/subscriptions/${subId}/providers/Microsoft.ContainerService/managedClusters?api-version=2023-05-02-preview`,
-    token
+    `https://management.azure.com/subscriptions/${sub}/providers/Microsoft.ContainerService/managedClusters?api-version=2023-05-02-preview`
   );
 }
 
-async function getACR(subId, token) {
+async function getACR(sub) {
   return armGet(
-    `https://management.azure.com/subscriptions/${subId}/providers/Microsoft.ContainerRegistry/registries?api-version=2023-01-01-preview`,
-    token
-  );
-}
-
-async function getStorage(subId, token) {
-  return armGet(
-    `https://management.azure.com/subscriptions/${subId}/providers/Microsoft.Storage/storageAccounts?api-version=2023-01-01`,
-    token
+    `https://management.azure.com/subscriptions/${sub}/providers/Microsoft.ContainerRegistry/registries?api-version=2023-01-01-preview`
   );
 }
